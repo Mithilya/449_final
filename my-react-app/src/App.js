5afd93a4478b9b255853461160ad3318c63fd1e2
Mithilya <113309804+Mithilya@users.noreply.github.com>
2024-04-21 21:49:42 +0000

    updated

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,7 +3,6 @@ import Axios from "axios";
 import React, { useEffect, useState } from 'react';
 
 
-
 function App() {
 
   const [catFact, setCatFact] = useState('');
@@ -47,26 +46,17 @@ function App() {
       <header className="App-header">
 
         <h1 className="text-3xl font-bold mb-4">Pet Facts Generator</h1>
-        <h2>do you have a pet, well it doesn't matter, learn about cats and dogs now by 
+        <h2>Do you have a pet? well it doesn't matter, learn about cats and dogs now by 
           generating facts. 
         </h2>
 
-        <p className="mb-2">Do you want to learn about cats? Generate facts.</p>
-        <button
-          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-          onClick={fetchCatFact}
-        >
-          Generate Cat Fact
-        </button>
+        <h3>Do you want to learn about cats? Generate facts.</h3>
+        <button onClick={fetchCatFact}> Generate Cat Fact </button>
         <p>{catFact}</p>
 
-        <p>Do you want to learn about dogs? Generate facts.</p>
-        <button
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-          onClick={fetchDogFact}
-        >
-          Generate Dog Fact
-        </button>
+        <h3>Do you want to learn about dogs? Generate facts.</h3>
+        <button onClick={fetchDogFact}
+        > Generate Dog Fact </button>
         <p>{dogFact}</p>
 
       </header>
