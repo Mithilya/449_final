a1f35b41c57aae6edafe5222bfda9d3861bf3752
Mithilya <113309804+Mithilya@users.noreply.github.com>
2024-04-21 20:54:06 +0000

    updated

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,11 +6,8 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const fetchData = () => {
-
-  }
-
   const [catFact, setCatFact] = useState('');
+  const [dogFact, setDogFact] = useState('');
 
   const fetchCatFact = () =>{
     Axios.get('https://catfact.ninja/fact')
@@ -19,27 +16,49 @@ function App() {
       });
   }
 
+  const fetchDogFact = () => {
+    Axios.get('https://dog-api.kinduff.com/api/facts')
+      .then((res) => {
+        const randomIndex = Math.floor(Math.random() * res.data.facts.length);
+        const randomFact = res.data.facts[randomIndex];
+        setDogFact(randomFact);
+      })
+  }
+
 
   useEffect(() =>{
     Axios.get('https://catfact.ninja/fact')
       .then((res) => {
         setCatFact(res.data.fact);
       });
-  },[])
+  },[]);
+
+  useEffect(() =>{
+    Axios.get('https://dog-api.kinduff.com/api/facts')
+      .then((res) => {
+        setDogFact(res.data.fact);
+      });
+  },[]);
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>RANDOM GENERATORS</h1>
-        <p>Do you want to learn about cats?</p>
+
+        <h1 className="text-3xl font-bold mb-4">Pet Facts Generator</h1>
+        <h2>do you have a pet, well it doesn't matter, learn about cats and dogs now by 
+          generating facts. 
+        </h2>
+
+        <p>Do you want to learn about cats? Generate facts.</p>
         <button onClick={fetchCatFact}>Generate Cat Fact</button>
         <p>{catFact}</p>
 
-        <input placeholder="Ex. Pedro..."/>
-        <button onClick={fetchData}>Predict Age</button>
-        
-        <h1>Predicted Age: </h1>
+        <p>Do you want to learn about dogs? Generate facts.</p>
+        <button onClick={fetchDogFact}>Generate Dog Fact</button>
+        <p>{dogFact}</p>
+
       </header>
     </div>
   );
